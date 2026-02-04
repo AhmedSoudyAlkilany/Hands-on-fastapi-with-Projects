0a1d248b6c6feecc09d3154e95e5691da8abbc45
Your Name <you@example.com>
2026-02-04 17:10:47 +0200

    Complete main file adding middleware and Exception handler

--- Project_1/app/main.py
+++ Project_1/app/main.py
@@ -1,7 +1,48 @@
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
-app = FastAPI(title="AI Text Analyzer")
+from app.routers import analyze
+
+
+app = FastAPI(
+    title="AI Text Analyzer",
+    description= "API لتحيل النصوص",
+    version="1.0.0"
+    )
+# إعدادات CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# Middleware لقياس الوقت
+@app.middleware("http")
+async def add_process_time(request: Request, call_next):
+    start = time.time()
+    response = await call_next(request)
+    response.headers["X-Process-Time"] = f"{(time.time() - start) * 1000:.2f}ms"
+    return response
+
+# Exception handler
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"error": str(exc)}
+    )
+
+
+# تسجيل Router
+app.include_router(analyze.router, prefix="/api/v1")
+
 
 @app.get("/")
 def root():
     return {"message": "Hello FastAPI"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}

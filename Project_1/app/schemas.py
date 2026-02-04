911d03677e9f8a1c2d51447b6051839282d710eb
Your Name <you@example.com>
2026-02-04 16:41:43 +0200

    adding Request and Responce schemas

--- Project_1/app/schemas.py
+++ Project_1/app/schemas.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from pydantic import BaseModel, Field
+from typing import Optional, List
 
 class SentimentType(str, Enum):
     POSITIVE = "positive"
@@ -16,3 +17,20 @@ class SentimentResult(BaseModel):
     type : SentimentType
     score : float = Field(..., ge=-1.0, le=1.0)
     confidence : float = Field(..., ge=0.0, le=1.0)
+
+class TextAnalysisRequest(BaseModel):
+    text : str = Field(..., min_length=10, max_length=10000)
+    include_keywords: bool = Field(default=True)
+    include_summary:bool = Field(default=False)
+    max_keywords: int = Field(default=5, ge=1, le=20)
+
+
+class TextAnalysisResponse(BaseModel):
+    original_text: str
+    language: LanguageCode
+    word_count: int
+    chatacter_count: int
+    sentiment: SentimentResult
+    keywords: Optional[List[str]] = None
+    summary: Optional[str] = None
+    processing_time_ms: float

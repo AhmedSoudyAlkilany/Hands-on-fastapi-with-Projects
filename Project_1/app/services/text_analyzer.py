ccec2312c7d4b6c1f4faf53cca8fb85e50a20b37
Your Name <you@example.com>
2026-02-04 16:33:58 +0200

    add analyze sentiment fuction to text_analyzer file

--- Project_1/app/services/text_analyzer.py
+++ Project_1/app/services/text_analyzer.py
@@ -1,5 +1,6 @@
 from langdetect import detect
-from app.schemas import LanguageCode
+from textblob import TextBlob
+from app.schemas import LanguageCode, SentimentType, SentimentResult
 
 class TextAnalyzerService:
     LANGUAGE_MAP = {
@@ -15,4 +16,17 @@ class TextAnalyzerService:
         except:
             return LanguageCode.UNKNOWN
         
+    def analyze_sentiment(self, text:str) -> SentimentResult:
+        blob = TextBlob(text)
+        polarity = blob.sentiment.polarity
+        if polarity > 0.1:
+            sentiment_type = SentimentType.POSITIVE
+        elif polarity < -0.1:
+            sentiment_type = SentimentType.NEGATIVE
+        else:
+            sentiment_type = SentimentType.NEUTRAL
+
+        confidence = min(abs(polarity) *2, 1.0)
+
+
 text_analyzer_service = TextAnalyzerService()

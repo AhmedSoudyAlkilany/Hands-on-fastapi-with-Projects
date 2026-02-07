cbc9ed69c226208b1eafcd82b3ab518fed6fcf7a
Your Name <you@example.com>
2026-02-07 03:37:53 +0200

    adding prompt class to models.py

--- Project_2/app/models.py
+++ Project_2/app/models.py
@@ -1,6 +1,7 @@
 "start with category"
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -11,3 +12,19 @@ class Category(Base):
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Prompt(Base):
+    __tablename__ = "prompts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(200), nullable=False, index=True)
+    content = Column(Text, nullable=False)
+    category_id = Column(Integer, ForeignKey("category.id") ,nullable=False)
+    category = relationship("Category", back_populates="prompts")
+    rating = Column(float, nullable=True)
+    usage_count = Column(Integer, default=0)
+    is_favorite = Column(bool, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    

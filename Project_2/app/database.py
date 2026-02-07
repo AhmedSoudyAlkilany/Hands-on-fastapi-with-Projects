6fb959a218f1c81b046236b6d5182f5ce18f119f
Your Name <you@example.com>
2026-02-07 03:52:00 +0200

    adding get_db function

--- Project_2/app/database.py
+++ Project_2/app/database.py
@@ -1,4 +1,8 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker
+)
 from sqlalchemy.orm import declarative_base
 
 from app.config import get_settings
@@ -10,6 +14,12 @@ engine = create_async_engine(
     echo=settings.debug,
 )
 
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
 Base = declarative_base()
 
 async def init_db():
@@ -17,3 +27,14 @@ async def init_db():
     async with engine.begin() as conn:
         from app import models
         await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_db():
+    "Dependency that provides a database session for each request."
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except:
+            await session.rollback()
+            raise

d0b4424f7273d15aef625f49bc3988cf9e468538
Your Name <you@example.com>
2026-02-08 01:44:05 +0200

    adding jwt to security.py file

--- Project_3/app/core/security.py
+++ Project_3/app/core/security.py
@@ -1,9 +1,39 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from typing import Optional, Dict, Any
+from app.config import get_settings
 
+settings = get_settings()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# password 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain: str, hashed: str) -> bool:
     return pwd_context.verify(plain, hashed)
+
+# JWT token
+def create_access_token(subject: str | int, extra: Dict = None) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.access_token_expire_minutes
+    )
+    
+    payload = {
+        "sub": str(subject),
+        "exp": expire,
+        "type": "access"
+    }
+    
+    if extra:
+        payload.update(extra)
+    
+    return jwt.encode(payload, settings.secret_key, algorithm=settings.algorithm)
+
+
+def decode_token(token: str) -> Optional[Dict]:
+    try:
+        return jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+    except JWTError:
+        return None

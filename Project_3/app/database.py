998643ae2348e859a3f1643977c1ca3667135a36
Your Name <you@example.com>
2026-02-08 01:57:17 +0200

    update database.py file

---
+++ Project_3/app/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from app.config import get_settings
+
+settings = get_settings()
+engine = create_async_engine(settings.database_url, echo=True)
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except:
+            await session.rollback()
+            raise
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        from app.models import user
+        await conn.run_sync(Base.metadata.create_all)
